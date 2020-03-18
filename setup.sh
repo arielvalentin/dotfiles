9841849ca1fef5144b45386a9e1d5639c29233a8
Ariel Valentin <ariel@arielvalentin.com>
2020-03-17 22:36:41 -0500

    dirty setup script

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+set -e
+
+echo "Show hidden files in finder"
+
+defaults write com.apple.Finder AppleShowAllFiles true && killall Finder
+
+echo "Install all homebrew dependencies"
+
+brew bundle
+
+echo "Lets write some shortcuts"
+
+(cd ~/ && ln -s bin/tmux.conf .tmux.conf &&
+  ln -s bin/gitignore_global .gitignore_global &&
+  ln -s bin/gitignore_global .ignore &&
+  ln -s bin/ctags.d .ctags.d)
+
+echo "Update zshrc"
+
+(cd ~/ && echo "source $HOME/bin/zshrc" >> .zshrc)
+
+echo "Configure nvim"
+
+(cd ~/ && mkdir -p ~/.config && ln -s bin/vim nvim)
