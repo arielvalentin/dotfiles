0c8f4f82e35f20774c0cca027f3970c5b53ce4bd
Ariel Valentin <ariel@arielvalentin.com>
2021-05-24 22:17:21 -0500

    another fix

--- setup.codespaces.sh
+++ setup.codespaces.sh
@@ -5,7 +5,7 @@ set -xe
 sudo apt-get install universal-ctags direnv jq ripgrep fzf
 
 current_dir=$(pwd)
-ln -sf $current_dir/.dotfiles/$file $HOME/.dotfiles
+ln -sf $current_dir/dotfiles $HOME/.dotfiles
 
 for file in tmux.conf gitignore_global ctags.d irbrc zshrc; do
   ln -sf $HOME/.dotfiles/$file $HOME/.$file
