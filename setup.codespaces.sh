d26c2b61b71aa70219c5195a8524731d2efd5c9a
Ariel Valentin <arielvalentin@users.noreply.github.com>
2021-06-26 09:42:50 -0500

    Update setup.codespaces.sh

--- setup.codespaces.sh
+++ setup.codespaces.sh
@@ -7,13 +7,12 @@ sudo apt-get install -y universal-ctags direnv jq ripgrep fzf
 current_dir=$(pwd)
 ln -sf $current_dir $HOME/.dotfiles
 
-for file in tmux.conf gitignore_global ctags.d irbrc zshrc; do
+for file in tmux.conf gitignore_global ctags.d irbrc; do
   ln -sf $HOME/.dotfiles/$file $HOME/.$file
 done
 
-(cd $HOME && ln -sf .gitignore_global .ignore)
+ln -sf $HOME/.dotfiles/zshrc.codespaces $HOME/.zshrc
 
-export EDITOR=vim
-export GIT_EDITOR=vim
+(cd $HOME && ln -sf .gitignore_global .ignore)
 
 git config --global commit.gpgsign true
